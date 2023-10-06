c6117db8bf9ec08d38c78a7cc6fb7aa8cbaaa289
ReTT27 <ivan.shevaykov@yandex.ru>
2023-10-06 19:17:08 +0300

    +upd

--- dictionary/tables/TypeRooms.sql
+++ dictionary/tables/TypeRooms.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS dictionary.typerooms
     type_id      SMALLSERIAL NOT NULL
         CONSTRAINT pk_typerooms PRIMARY KEY,
     name         VARCHAR(64) NOT NULL,
-    number_beds  SMALLINT    NOT NULL,
+    number_beds  VARCHAR(4)  NOT NULL,
     number_rooms SMALLINT    NOT NULL,
     cost         INT         NOT NULL
 );
