80f925a0f998442a15950ee5568bce0ebdcfe234
ReTT27 <ivan.shevaykov@yandex.ru>
2023-10-09 13:09:20 +0300

    Удаление схемы history

--- history/tables/TypePaymentChanges.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS history.typepaymentchanges
-(
-    log_id      BIGSERIAL   NOT NULL
-        CONSTRAINT pk_typepaymentchanges PRIMARY KEY,
-    payment_id  SMALLSERIAL NOT NULL,
-    name        VARCHAR(32) NOT NULL,
-    dt_ch       TIMESTAMPTZ NOT NULL,
-    ch_employee INT         NOT NULL
-);
--- history/tables/TypeTimelineChanges.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS history.typetimelinechanges
-(
-    log_id      BIGSERIAL   NOT NULL
-        CONSTRAINT pk_typetimelinechanges PRIMARY KEY,
-    timeline_id SMALLINT    NOT NULL,
-    name        VARCHAR(32) NOT NULL,
-    dt_ch       TIMESTAMPTZ NOT NULL,
-    ch_employee INT         NOT NULL
-);
