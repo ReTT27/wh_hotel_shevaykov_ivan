7ef8e5d3ebcbba5169c626a867b52fa5792bdae5
ReTT27 <ivan.shevaykov@yandex.ru>
2023-10-13 18:14:31 +0300

    +upd

---
+++ dictionary/functions/PositionUpd.sql
@@ -0,0 +1,43 @@
+CREATE OR REPLACE FUNCTION dictionary.positionupd(_src JSONB) RETURNS JSONB
+    SECURITY DEFINER
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    _position_id SMALLINT;
+    _name    VARCHAR(64);
+    _salary  NUMERIC(7, 2);
+BEGIN
+
+    SELECT COALESCE(p.post_id, nextval('dictionary.positionsq')) AS post_id,
+           s.name,
+           s.salary
+    INTO _post_id,
+         _name,
+         _salary
+    FROM jsonb_to_record(_src) AS s (post_id SMALLINT,
+                                     name    VARCHAR(64),
+                                     salary  NUMERIC(7, 2))
+             LEFT JOIN dictionary.position p
+                       ON p.post_id = _post_id;
+
+    IF (_salary < 0 AND _salary IS NOT NULL )
+    THEN
+        RETURN public.errmessage(_errcode := 'dictionary.position_ins.salary',
+                                 _msg     := 'Зарплата не может быть отрицательной!',
+                                 _detail  := concat('salary = ', _salary));
+    END IF;
+
+    INSERT INTO dictionary.position AS p (post_id,
+                                          name,
+                                          salary)
+    SELECT _post_id,
+           _name,
+           _salary
+    ON CONFLICT (post_id) DO UPDATE
+        SET name   = excluded.name,
+            salary = excluded.salary;
+
+    RETURN JSONB_BUILD_OBJECT('data', NULL);
+END
+$$;
