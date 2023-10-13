73e4afa5f922d313e571fea2f277e0a30f2615f4
ReTT27 <ivan.shevaykov@yandex.ru>
2023-10-13 20:05:51 +0300

    +upd

--- dictionary/tables/TypeFeed.sql
+++ dictionary/tables/TypeFeed.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS dictionary.typefeed
         CONSTRAINT pk_typefeed PRIMARY KEY,
     name        VARCHAR(32)   NOT NULL,
     content     VARCHAR(128)  NOT NULL,
-    cost        NUMERIC(6, 2) NOT NULL,
+    cost        NUMERIC(7, 2) NOT NULL,
     CONSTRAINT ch_typefeed_cost CHECK ( cost > 0 )
 );
--- hotel/tables/Employee.sql
+++ hotel/tables/Employee.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS hotel.employee
     phone       VARCHAR(11)   NOT NULL,
     email       VARCHAR(32)   NOT NULL,
     position_id SMALLINT      NOT NULL,
-    reward      NUMERIC(7, 2) NOT NULL,
+    reward      NUMERIC(8, 2) NOT NULL,
     is_deleted  BOOLEAN DEFAULT FALSE,
     ch_employee INT           NOT NULL,
     dt_ch       TIMESTAMPTZ   NOT NULL
