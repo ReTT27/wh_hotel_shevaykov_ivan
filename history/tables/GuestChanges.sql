8fc6c4080400a48332099415ae7adfbe8d3237a7
ReTT27 <ivan.shevaykov@yandex.ru>
2023-10-17 14:56:48 +0300

    +upd

---
+++ history/tables/GuestChanges.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS history.guestchanges
+(
+    lord_id     BIGSERIAL   NOT NULL
+        CONSTRAINT pk_guestchanges PRIMARY KEY,
+    guest_id    INT         NOT NULL,
+    name        VARCHAR(64) NOT NULL,
+    phone       VARCHAR(11) NOT NULL,
+    email       VARCHAR(32) NOT NULL,
+    birth_day   DATE        NOT NULL,
+    card_id     INT,
+    ch_employee INT         NOT NULL,
+    dt_ch       TIMESTAMPTZ NOT NULL
+);
