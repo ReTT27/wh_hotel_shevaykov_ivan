9c565e743ae1c36f9bf798f664d124613a29c82c
ReTT27 <ivan.shevaykov@yandex.ru>
2023-10-19 13:04:11 +0300

    +upd

--- history/functions/DeletePartitionsCleaning.sql
+++ history/functions/DeletePartitionsCleaning.sql
@@ -1,23 +1,23 @@
-CREATE OR REPLACE FUNCTION history.deletepartitionscleaning() RETURNS JSONB
+CREATE OR REPLACE FUNCTION history.deletepartitionscleaning(_name_inh TEXT) RETURNS JSONB
     SECURITY DEFINER
     LANGUAGE plpgsql
 AS
 $$
 DECLARE
-    _name VARCHAR(64)[];
+    _name_part VARCHAR(64)[];
 BEGIN
 
     SELECT array_agg(c.relname)
-    INTO _name
-    FROM pg_class c
-    WHERE extract(month from split_part(pg_get_expr(c.relpartbound, c.oid, true), 'TO', 2)::DATE) <
-          extract(month from now() - interval '3 month')
-      AND extract(years from split_part(pg_get_expr(c.relpartbound, c.oid, true), 'TO', 2)::DATE) =
-          extract(years from now());
+    INTO _name_part
+    FROM pg_catalog.pg_inherits i
+             INNER JOIN pg_catalog.pg_class c on c.oid = i.inhrelid
+    WHERE date_trunc('month', split_part(pg_get_expr(c.relpartbound, c.oid, true), 'TO', 2)::DATE) <=
+          date_trunc('month', now() - interval '3 month')::DATE
+      AND i.inhparent::regclass::TEXT = concat('history.',_name_inh);
 
-    FOR i IN 1..array_length(_name, 1)
+    FOR i IN 1..array_length(_name_part, 1)
         LOOP
-             EXECUTE format('DROP TABLE history.%I', _name[i]);
+             EXECUTE format('DROP TABLE history.%I', _name_part[i]);
         END LOOP;
 
     RETURN JSONB_BUILD_OBJECT('data', null);
