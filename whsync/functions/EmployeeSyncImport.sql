f138309eee75ee4094df1334889c63adb0a81013
ReTT27 <ivan.shevaykov@yandex.ru>
2023-10-20 17:06:44 +0300

    +upd

--- whsync/functions/EmployeeSyncImport.sql
+++ whsync/functions/EmployeeSyncImport.sql
@@ -24,17 +24,46 @@ BEGIN
                                                         is_deleted  BOOLEAN,
                                                         ch_employee INT,
                                                         dt_ch       TIMESTAMPTZ))
+       , inc_t AS (INSERT INTO hotel.employee AS e (employee_id,
+                                                    name,
+                                                    phone,
+                                                    email,
+                                                    position_id,
+                                                    reward,
+                                                    is_deleted,
+                                                    ch_employee,
+                                                    dt_ch)
+                   SELECT ic.employee_id,
+                          ic.name,
+                          ic.phone,
+                          ic.email,
+                          ic.position_id,
+                          ic.reward,
+                          ic.is_deleted,
+                          ic.ch_employee,
+                          ic.dt_ch
+                   FROM ins_cte ic
+                   WHERE ic.rn = 1
+                   ON CONFLICT (employee_id) DO UPDATE
+                        SET name        = excluded.name,
+                            phone       = excluded.phone,
+                            email       = excluded.email,
+                            position_id = excluded.position_id,
+                            reward      = excluded.reward,
+                            is_deleted  = excluded.is_deleted,
+                            dt_ch       = excluded.dt_ch,
+                            ch_employee = excluded.ch_employee
+                   WHERE e.dt_ch <= excluded.dt_ch)
 
-
-    INSERT INTO hotel.employee AS e (employee_id,
-                                     name,
-                                     phone,
-                                     email,
-                                     position_id,
-                                     reward,
-                                     is_deleted,
-                                     ch_employee,
-                                     dt_ch)
+    INSERT INTO history.employeechanges AS ec (employee_id,
+                                               name,
+                                               phone,
+                                               email,
+                                               position_id,
+                                               reward,
+                                               is_deleted,
+                                               ch_employee,
+                                               dt_ch)
     SELECT ic.employee_id,
            ic.name,
            ic.phone,
@@ -55,7 +84,7 @@ BEGIN
             is_deleted  = excluded.is_deleted,
             dt_ch       = excluded.dt_ch,
             ch_employee = excluded.ch_employee
-    WHERE e.dt_ch <= excluded.dt_ch;
+    WHERE ec.dt_ch <= excluded.dt_ch;
 
     RETURN JSONB_BUILD_OBJECT('data',NULL);
 END
