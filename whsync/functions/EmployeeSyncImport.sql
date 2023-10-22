b87a34d5d7a7cbb432814ddfc24b8f2d760757fc
ReTT27 <ivan.shevaykov@yandex.ru>
2023-10-22 18:44:56 +0300

    +upd

--- whsync/functions/EmployeeSyncImport.sql
+++ whsync/functions/EmployeeSyncImport.sql
@@ -53,7 +53,8 @@ BEGIN
                             is_deleted  = excluded.is_deleted,
                             dt_ch       = excluded.dt_ch,
                             ch_employee = excluded.ch_employee
-                   WHERE e.dt_ch <= excluded.dt_ch)
+                   WHERE e.dt_ch <= excluded.dt_ch
+                   RETURNING e.*)
 
     INSERT INTO history.employeechanges AS ec (employee_id,
                                                name,
@@ -64,27 +65,16 @@ BEGIN
                                                is_deleted,
                                                ch_employee,
                                                dt_ch)
-    SELECT ic.employee_id,
-           ic.name,
-           ic.phone,
-           ic.email,
-           ic.position_id,
-           ic.reward,
-           ic.is_deleted,
-           ic.ch_employee,
-           ic.dt_ch
-    FROM ins_cte ic
-    WHERE ic.rn = 1
-    ON CONFLICT (employee_id) DO UPDATE
-        SET name        = excluded.name,
-            phone       = excluded.phone,
-            email       = excluded.email,
-            position_id = excluded.position_id,
-            reward      = excluded.reward,
-            is_deleted  = excluded.is_deleted,
-            dt_ch       = excluded.dt_ch,
-            ch_employee = excluded.ch_employee
-    WHERE ec.dt_ch <= excluded.dt_ch;
+    SELECT it.employee_id,
+           it.name,
+           it.phone,
+           it.email,
+           it.position_id,
+           it.reward,
+           it.is_deleted,
+           it.ch_employee,
+           it.dt_ch
+    FROM inc_t it;
 
     RETURN JSONB_BUILD_OBJECT('data',NULL);
 END
