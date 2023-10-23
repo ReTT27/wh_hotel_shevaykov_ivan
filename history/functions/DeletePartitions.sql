8de2a82e3c8aa5f021194e0a63e0473738c84c97
ReTT27 <ivan.shevaykov@yandex.ru>
2023-10-23 13:00:37 +0300

    +upd

--- history/functions/DeletePartitions.sql
+++ history/functions/DeletePartitions.sql
@@ -7,27 +7,26 @@ DECLARE
     _name_part VARCHAR(64)[];
 BEGIN
 
-    SELECT array_agg(c.relname)
+    SELECT ARRAY_AGG(c.relname)
     INTO _name_part
     FROM pg_catalog.pg_inherits i
              INNER JOIN pg_catalog.pg_class c on c.oid = i.inhrelid
-    WHERE date_trunc('month', split_part(pg_get_expr(c.relpartbound, c.oid, true), 'TO', 2)::DATE) <=
-          date_trunc('month', now() - interval '3 month')::DATE
-      AND i.inhparent::regclass::TEXT = concat('history.',_name_inh);
+    WHERE DATE_TRUNC('month', SPLIT_PART(PG_GET_EXPR(c.relpartbound, c.oid, TRUE), 'TO', 2)::DATE) <=
+          DATE_TRUNC('month', NOW() - INTERVAL '3 month')::DATE
+      AND i.inhparent::REGCLASS::TEXT = CONCAT('history.',_name_inh);
 
-    IF (array_length(_name_part, 1) IS NULL)
+    IF (ARRAY_LENGTH(_name_part, 1) IS NULL)
     THEN
         RETURN public.errmessage(_errcode := 'history.partitions_del',
                                  _msg     := 'Таблиц для удаления нет!',
                                  _detail  := 'Длина массива = 0');
     END IF;
 
-    FOR i IN 1..array_length(_name_part, 1)
+    FOR i IN 1..ARRAY_LENGTH(_name_part, 1)
         LOOP
-             EXECUTE format('DROP TABLE history.%I', _name_part[i]);
+             EXECUTE FORMAT('DROP TABLE history.%I', _name_part[i]);
         END LOOP;
 
     RETURN JSONB_BUILD_OBJECT('data', null);
-
 END
 $$;
